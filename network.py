Interactions with less bias for class-class. Tail on off-diag and wrong shape for class-class

network.py
@@ -8,21 +8,23 @@ import pickle
 import sys
 from scipy.optimize import least_squares, dual_annealing, leastsq
 from scipy.optimize import minimize
+from sklearn.ensemble import GradientBoostingClassifier
 from analysis import Analysis
+import matplotlib.pyplot as plt
 
 sys.setrecursionlimit(10000)
 from person import Person, Interaction
 
 
 class Network:
-    def __init__(self, num_students, num_grades, num_classes, class_threshhold=20, parameterList=[]):
+    def __init__(self, num_students, num_grades, num_classes, class_treshold=20, parameterList=[]):
         self.parameterList = parameterList
-        self.students = self.generate_students(num_students, num_grades, num_classes, class_treshold=class_threshhold)
+        self.students = self.generate_students(num_students, num_grades, num_classes, class_treshold=class_treshold)
         self.d = (1) * pow(10, -2)  # 4.3
         self.graph = self.generate_network()
         self.daily_list = []
 
-    def generate_students(self, num_students, num_grades, num_classes, class_treshold=20, param=False):
+    def generate_students(self, num_students, num_grades, num_classes, class_treshold=20):
         available_grades = [i for i in range(1, num_grades + 1)]
         available_classes = [chr(i) for i in range(97, 97 + num_classes)]
 
@@ -30,7 +32,6 @@ class Network:
 
         for grade in available_grades:  # Loop igjennom antall grades
             i = 0
-            has_filled = False  # Bare et flagg for å sjekke om vi har gjort en random fylling av klasser eller ikke
             for pers in range(1, num_students // num_grades + 1):  # Loop igjennom antall personer pr grade
                 students.append(Person(grade, available_classes[i]))  # Legg til person i students
                 if (
@@ -44,13 +45,14 @@ class Network:
                     elif i >= len(
                         available_classes
                     ):  # Hvis vi ikke har fler enn class_threshold studenter igjen legger vi de i en random klasse av de vi allerede har
-                        has_filled = True  # "Si ifra" til loopen at vi har gjort en random fylling av studentente.
-                        for _ in range(num_students // num_grades - pers):
-                            students.append(
-                                Person(grade, random.choice(available_classes))
-                            )  # Legg til studenter i randome klasser
-                if has_filled:  # Break dersom vi har fylt random
-                    break
+                        break
+
+        grade_ind = 0
+        while len(students) != num_students:
+            if grade_ind >= len(available_grades):
+                grade_ind = 0
+            students.append(Person(available_grades[grade_ind], random.choice(available_classes)))
+            grade_ind += 1
 
         students = sorted(students, key=lambda x: x.get_class_and_grade())
 
@@ -93,7 +95,9 @@ class Network:
 
                 tentative_weight = np.random.poisson(stud.p_vector[pers] * self.d)  # random.poisson
 
-                if tentative_weight < 200:
+                if (
+                    tentative_weight < 180
+                ):  # It is only possible to interact 180 times an hour (if each interaction is maxumum 20 seconds long 60*60/20)
                     weight = tentative_weight
 
                 if weight:
@@ -148,149 +152,157 @@ class Network:
         file_to_read = open(name, "rb")
         return pickle.load(file_to_read)
 
+    ### Estimation
     def parameterEstimation(self):
-
-        # X0 = [20, 1, 40, 1, 60, 1, 80, 1]
-        # X0 = [4.0115483, 0.05900369, 0.10502307, 0.74691569]
-        X0 = [10, 10, 10, 10]
-        bounds = ([0 for i in range(4)], [np.inf for i in range(4)])
-        # bounds = [(1e-15, 10000), (1e-15, 10000), (1e-15, 10000), (1e-15, 10000)]
-
-        # experimentalDataDict = self.pickleLoad("graph1_whole_pixel.pkl")
-        # exp_class_class = self.pickleLoad("graph1_class_pixel.pkl")
-        # exp_grade_grade = self.pickleLoad("graph1_grade_pixel.pkl")
-        exp_offDiag = self.pickleLoad("graph1_off_diag.pkl")
-
         def createSubGraphWithoutGraph(graph, diagonal, gradeInteraction):
             G = nx.Graph()
 
-            for node in graph:
+            for node in graph.nodes():
                 klasseAttr = node.getClass()
-                for n in graph.neighbors(node):
+                for n in graph.nodes():
                     if diagonal:
                         if n.getClass() == klasseAttr and node.getGrade() == n.getGrade():
-                            G.add_edge(node, n, count=graph.get_edge_data(node, n)["count"])
-                            G.add_node(n)
+                            if n in graph.neighbors(node):
+                                G.add_edge(node, n, count=graph.get_edge_data(node, n)["count"])
+                                G.add_node(n)
+                                G.add_node(node)
+                            else:
+                                G.add_edge(node, n, count=0)
+                                G.add_node(n)
+                                G.add_node(node)
                     if gradeInteraction:
-                        if n.getGrade() == node.getGrade() and node.getClass() != n.getClass():
-                            G.add_edge(node, n, count=graph.get_edge_data(node, n)["count"])
-                            G.add_node(n)
-            # self.heatmap(G)
+                        if n.getGrade() == node.getGrade() and klasseAttr != n.getClass():
+                            if n in graph.neighbors(node):
+                                G.add_edge(node, n, count=graph.get_edge_data(node, n)["count"])
+                                G.add_node(n)
+                                G.add_node(node)
+                            else:
+                                G.add_edge(node, n, count=0)
+                                G.add_node(n)
+                                G.add_node(node)
+                    else:
+                        G.add_edge(node, n, count=0)
+                        G.add_node(n)
+                        G.add_node(node)
+
             return G
 
         def createSubGraphWithout(graph, grade, klasse):
             G = nx.Graph()
-
-            for node in graph:
-                for n in graph.neighbors(node):
+            # print("this is the subgraph before anything: " + str(graph))
+            for node in graph.nodes():
+                for n in graph.nodes():
                     if grade and not klasse:
                         if node.getGrade() != n.getGrade():
-                            G.add_edge(node, n, count=graph[node][n]["count"])
+                            if n in graph.neighbors(node):
+                                G.add_edge(node, n, count=graph.get_edge_data(node, n)["count"])
+                                G.add_node(n)
+                                G.add_node(node)
+                            else:
+                                G.add_edge(node, n, count=0)
+                                G.add_node(n)
+                                G.add_node(node)
                     elif not grade:
                         if node.get_class_and_grade() != n.get_class_and_grade():
-                            G.add_edge(node, n, count=graph[node][n]["count"])
-            # self.heatmap(G)
+                            if n in graph.neighbors(node):
+                                G.add_edge(node, n, count=graph.get_edge_data(node, n)["count"])
+                                G.add_node(n)
+                                G.add_node(node)
+                            else:
+                                G.add_edge(node, n, count=0)
+                                G.add_node(n)
+                                G.add_node(node)
+                    else:
+                        G.add_edge(node, n, count=0)
+                        G.add_node(n)
+                        G.add_node(node)
+
             return G
 
-        def toCumulative(l):
-            n = len(l)
-            dictHist = {}
-            for i in l:
-                if i not in dictHist:
-                    dictHist[i] = 1
-                else:
-                    dictHist[i] += 1
-            cHist = {}
-            cumul = 1
-            for i in dictHist:
-                cHist[i] = cumul
-                cumul -= float(dictHist[i]) / float(n)
-            return cHist
+        def load_all_pixeldist_non_cumulative():
+            exp_whole = self.pickleLoad("graph1_whole_pixel_not_Cumulative.pkl")
+            exp_diag = self.pickleLoad("graph1_off_diag_not_Cumulative.pkl")
+            exp_class = self.pickleLoad("graph1_class_pixel_not_Cumulative.pkl")
+            exp_grade = self.pickleLoad("graph1_grade_pixel_not_Cumulative.pkl")
 
-        def toArray(sim, exp):
-            Sim_Adj = nx.adjacency_matrix(sim, nodelist=sorted(sim.nodes), weight="count")
-            length = len(sim.nodes())
+            return exp_whole, exp_diag, exp_class, exp_grade
+
+        def rank_interaction(pixel):
+            pixel[::-1].sort()
+            pixel_list = pixel.tolist()
+
+            pixel_dict = {}
+
+            for i in range(len(pixel_list) - 1):
+                pixel_dict[i] = pixel_list[i]
+            pixel_dict = np.array(list(pixel_dict.values()))
+            return pixel_dict
+
+        def setup():
+            exp_whole, exp_diag, exp_class, exp_grade = load_all_pixeldist_non_cumulative()
+            exp_whole_dict = rank_interaction(exp_whole)
+            exp_diag_dict = rank_interaction(exp_diag)
+            exp_class_dict = rank_interaction(exp_class)
+            exp_grade_dict = rank_interaction(exp_grade)
+
+            return exp_whole_dict, exp_diag_dict, exp_class_dict, exp_grade_dict
+
+        def toSorted(graph):
+            Sim_Adj = nx.adjacency_matrix(graph, nodelist=sorted(graph.nodes), weight="count")
+            length = len(graph.nodes())
 
             weights = Sim_Adj[np.triu_indices(length, k=1)].tolist()[0]
 
-            simData = sorted(weights)
+            simData = np.array(sorted(weights))
 
-            sortedSim = np.sort(simData)
+            return rank_interaction(simData)
 
-            simCum = toCumulative(sortedSim)
+        exp_whole, exp_diag, exp_class, exp_grade = setup()
 
-            new_Sim = simCum.copy()
+        def parameters(X):
+            def power_of_2(x):
+                return np.power(x, 2)
 
-            new_exp = exp.copy()
-
-            same = set(list(new_exp.keys())).intersection(set(list(new_Sim.keys())))
-
-            keys = list(new_exp.keys())
-
-            for key in keys:
-                if key not in same:
-                    del new_exp[key]
-
-            f_keys = list(new_Sim.keys())
-            for key in f_keys:
-                if key not in same:
-                    del new_Sim[key]
-
-            y = np.array(list(new_exp.items()))
-            F = np.array(list(new_Sim.items()))
-
-            return y, F
-
-        def objectiveFunc(X):
-            ## Off-diagonal excluding lunch
-            # a1 = X[0]
-            # b1 = X[1]
-
-            ## Off-diagonal with lunch
-            # a2 = X[2]
-            # b2 = X[3]
-
-            ## Grade-Grade
-            # a3 = X[4]
-            # b3 = X[5]
-
-            ## Class-Class
-            # a4 = X[6]
-            # b4 = X[7]
-
-            # graph = self.generate_a_day(param=True)
-            # graph = self.daily_list[-1]
-            graph = Network(225, 5, 2, parameterList=X).generate_a_day()
+            graph = Network(236, 5, 2, class_treshold=23, parameterList=X).generate_a_day()
 
             off_diagonal = createSubGraphWithout(graph, True, False)
             # grade_grade = createSubGraphWithoutGraph(graph, False, True)
             # class_class = createSubGraphWithoutGraph(graph, True, False)
 
-            # exp, sim = toArray(graph, experimentalDataDict)
-            exp_diag, sim_diag = toArray(off_diagonal, exp_offDiag)
-            # sim_grade, exp_grade = toArray(grade_grade, exp_grade_grade)
-            # sim_class, exp_class = toArray(class_class, exp_class_class)
+            # print(f"OFf diag: {off_diagonal}, grade_grade: {grade_grade}, class_class: {class_class}")
 
-            # diff_whole = exp[:, 1][:20] - sim[:, 1][:20]  # 85
+            # whole_dict = toSorted(graph)
+            diag_dict = toSorted(off_diagonal)
+            # grade_dict = toSorted(grade_grade)
+            # class_dict = toSorted(class_class)
 
-            diff_diag = exp_diag[:, 1] - sim_diag[:, 1]  # 25
-            # diff_grade = exp_grade[:, 1][:20] - sim_grade[:, 1][:20]  # 20
-            # diff_class = exp_class[:, 1][:20] - sim_class[:, 1][:20]  # 50
-            diff_diag
-            print(diff_diag)
+            raise_2 = np.vectorize(power_of_2)
 
-            return diff_diag
-            # res = (np.abs(diff_diag)).sum()  # diff_whole + diff_diag + diff_grade + diff_class
-            # print(res)
-            # return res
+            output = raise_2(exp_diag - diag_dict)
+            # raise_2(exp_whole - whole_dict)+ raise_2(exp_diag - diag_dict)+ raise_2(exp_grade - grade_dict)+ raise_2(exp_class - class_dict))
+            output = output.sum()
+            return output
 
-        result = least_squares(
-            objectiveFunc, X0, method="trf", loss="cauchy", bounds=bounds, ftol=1e-10, xtol=1e-15
-        )  # loss="cauchy"
-        print(result)
+        def loop_parameters(low, top, step):
+            i = 0
+            dict_results = {}
+            res = 0
+            for _ in range(20):
+                parameter = [3, 0.1, 9, 0.01, 1, 1, 1, 1]
+                res += parameters(parameter)
+
+            print(i)
+            i += 1
+            dict_results[i] = [(res / 20), parameter]
+            print("res: " + str(res) + " parametere: " + str(parameter))
+            # print(min(dict_results.values()[0]))
+            # print(min(dict_results.values()[1]))
+
+        loop_parameters(3, 9, 0.1)
+        # print(parameters([]))
 
 
 if __name__ == "__main__":
-    network = Network(225, 5, 2)
+    network = Network(num_students=236, num_grades=5, num_classes=2, class_treshold=23)
     # G = network.generateXdays(8)
     network.parameterEstimation()

person.py
@@ -12,9 +12,6 @@ from enums import Grade
 from layers import Grades, Klasse, Lunchbreak, Recess
 
 
-# decimal.getcontext().prec = 12
-
-
 class Person:
     newid = itertools.count()
 
@@ -98,7 +95,6 @@ class Person:
     def generate_p_vector(self, students, X):
         rand = True
         if len(X):
-            rand = True
             ## Off-diagonal excluding lunch
             a1 = X[0]
             b1 = X[1]
@@ -116,19 +112,19 @@ class Person:
             b4 = 0.1  # X[7]
         else:
             ## Off-diagonal excluding lunch
-            a1 = 0.2  # 1.5
-            b1 = 3.5  # 1.25
+            a1 = 0.4  # 1.5
+            b1 = 0.5  # 0.1
             ##  Off-diagonal with lunch
-            a2 = 0.01  # 0.001
-            b2 = 0.01  # 0.07
+            a2 = 0.3  # 0.001
+            b2 = 0.5  # 0.07
 
             ## Grade-Grade
-            a3 = 3  # 100
-            b3 = 2  # 0.3
+            a3 = 3.5  # 100
+            b3 = 0.8  # 0.3
 
             ## Class-Class
-            a4 = 6  # 10000
-            b4 = 3  # 1
+            a4 = 25  # 10000
+            b4 = 16  # 1
 
         for i in range(len(students)):
             same_lunch = self.lunch_group == students[i].lunch_group
@@ -136,20 +132,26 @@ class Person:
             same_class = self.class_group == students[i].class_group and self.grade == students[i].grade
 
             p = a1 * np.random.power(b1)
+            # p = 0.1
 
             ### Lunch: Off-diagonal boosted with lunchgroups
             if same_lunch:
                 p += a2 * np.random.power(b2)
+                # p += 0.15
 
             ### Grade-grade interaction layer
             if same_grade:
                 p += a3 * np.random.power(b3)
+                # p += 0.4
 
             ### Class-class interaction layer. Assume no bias/low bias for class-class interactions. No free-time activity
             if same_class:
+
                 p += a4 * np.random.power(b4)
-                self.p_vector[students[i]] = p
-                continue
+                self.bias_vector[students[i]] = 20 * (math.log10(1 / random.random()))
+                students[i].bias_vector[self] = 20 * (math.log10(1 / random.random()))
+
+                # self.p_vector[students[i]] = p * 10
 
             self.p_vector[students[i]] = p * self.bias_vector[students[i]] * students[i].bias_vector[self]
 