c0e62c5067a2c6cbba5dc2cea2c25705a137e6ca
SaraAsche <saraja@stud.ntnu.no>
2022-05-05 15:31:28 +0200

    final testing run

--- disease_transmission.py
+++ disease_transmission.py
@@ -1207,20 +1207,20 @@ if __name__ == "__main__":
     # )
 
     # testing
-    disease_transmission.plot_recovered(
-        "./data/weekly_testing2/not_tested_average.csv", show=False, lab="Not tested", colour="rosybrown"
-    )
-    disease_transmission.plot_recovered(
-        "./data/weekly_testing2/tested_weekly_average.csv",
-        show=False,
-        lab="Weekly tested",
-        colour="darkseagreen",
-        tested=True,
-    )
-    disease_transmission.plot_recovered(
-        "./data/weekly_testing2/tested_biweekly_average.csv",
-        show=True,
-        lab="Biweekly tested",
-        colour="darkgoldenrod",
-        tested=True,
-    )
+    # disease_transmission.plot_recovered(
+    #     "./data/weekly_testing2/not_tested_average.csv", show=False, lab="Not tested", colour="rosybrown"
+    # )
+    # disease_transmission.plot_recovered(
+    #     "./data/weekly_testing2/tested_weekly_average.csv",
+    #     show=False,
+    #     lab="Weekly tested",
+    #     colour="darkseagreen",
+    #     tested=True,
+    # )
+    # disease_transmission.plot_recovered(
+    #     "./data/weekly_testing2/tested_biweekly_average.csv",
+    #     show=True,
+    #     lab="Biweekly tested",
+    #     colour="darkgoldenrod",
+    #     tested=True,
+    # )
